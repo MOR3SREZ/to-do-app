aa214f4a3331a88d9bc928d80cbf1b96300d7fb0
morteza rezaeian <morteza78rezaeian@gmail.com>
2021-12-15 14:50:07 +0330

    all styles Done!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import Do from './components/Do';
 function App() {
   return (
-    <div className="App">
- 
+    <div className='App'>
+      <div className='todo-app'>
+        <h1>ToDo App</h1>
+        <div className='todo-input'>
+          <input type='text' />
+          <button className='add-btn'>ADD</button>
+        </div>
+      </div>
+      <Do />
+      <Do />
+      <Do />
     </div>
   );
 }
