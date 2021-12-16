16b7c218b99745eb1a4861edb136f47a0249e560
MOR3SREZ <69503729+MOR3SREZ@users.noreply.github.com>
2021-12-16 23:27:07 +0330

    to do list without component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,89 @@
+import { useState } from 'react';
+
 import './App.css';
-import Do from './components/Do';
+
 function App() {
+  //start Hooks
+  const [input, setInputs] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  // start functions
+  const changeHandler = (e) => {
+    setInputs(e.target.value);
+  };
+  let id = null;
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    id = Math.floor(Math.random() * 10000);
+    const obj = {
+      id: id,
+      text: input,
+    };
+    if (!obj.text || /^\s*$/.test(obj.text)) {
+      return;
+    } else {
+      todoList.push(obj);
+      setTodoList([...todoList]);
+    }
+
+    setInputs('');
+  };
+
+  const romoveHandler = (id) => {
+    const removedArr = [...todoList].filter((e) => e.id !== id);
+    setTodoList(removedArr);
+  };
+  const completeHandler = (id) => {
+    let updatedTodos = [...todoList].map((todo) => {
+      if (todo.id === id) {
+        todo.completeHandler = !todo.completeHandler;
+      }
+      return todo;
+    });
+    setTodoList(updatedTodos);
+  };
+
+  // start Return
   return (
     <div className='App'>
       <div className='todo-app'>
         <h1>ToDo App</h1>
-        <div className='todo-input'>
-          <input type='text' />
+        <form className='todo-input' onSubmit={submitHandler}>
+          <input
+            placeholder='Add a todo'
+            value={input}
+            type='text'
+            onChange={changeHandler}
+          />
           <button className='add-btn'>ADD</button>
-        </div>
+        </form>
       </div>
-      <Do />
-      <Do />
-      <Do />
+
+      <div className='works-todo'>
+        {todoList.map((e) => (
+          <div
+            className={e.completeHandler ? 'works complete' : 'works'}
+            key={e.id}
+          >
+            <p className='work'>{e.text}</p>
+            <div className='works-btn'>
+              <button
+                className='done-btn'
+                onClick={() => completeHandler(e.id)}
+              >
+                ✅
+              </button>
+              <button
+                className='not-done-btn'
+                onClick={() => romoveHandler(e.id)}
+              >
+                ❌
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
